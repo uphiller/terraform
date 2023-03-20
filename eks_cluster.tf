5dcb73c7b499c7ed346446d3f0f82470885a12b7
YoungHo Kwak <jason.uphiller@gmail.com>
2023-03-20 14:22:25 +0900

    Update eks_cluster.tf

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,17 +1,9 @@
-data "terraform_remote_state" "vpc" {
-  backend = "local"
-
-  config = {
-    path = "terraform.tfstate"
-  }
-}
-
 resource "aws_eks_cluster" "shop" {
   name    = "shop"
   role_arn = "arn:aws:iam::193945198166:role/EKS"
 
   vpc_config {
-    subnet_ids = [data.terraform_remote_state.vpc.outputs.subnet1]
+    subnet_ids = [aws_subnet.first.id, aws_subnet.second.id]
   }
 }
 
