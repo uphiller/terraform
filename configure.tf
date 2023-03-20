546622dcdec62f22fcad519da236299ca28c3c24
YoungHo Kwak <jason.uphiller@gmail.com>
2023-03-20 11:49:29 +0900

    Update configure.tf

--- configure.tf
+++ configure.tf
@@ -1,3 +1,11 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
 provider "aws" {
   access_key =  var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
