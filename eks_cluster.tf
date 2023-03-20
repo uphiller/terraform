a4c7a5b69f5e471f9e9b0cb5d76bf148285da775
YoungHo Kwak <jason.uphiller@gmail.com>
2023-03-20 13:50:35 +0900

    Create eks_cluster.tf

---
+++ eks_cluster.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "ap-northeast-2"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "terraform.tfstate"
+  }
+}
+
+resource "aws_eks_cluster" "shop" {
+  name    = "shop"
+  role_arn = "arn:aws:iam::193945198166:role/EKS"
+
+  vpc_config {
+    subnet_ids = [data.terraform_remote_state.vpc.outputs.subnet1]
+  }
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.shop.endpoint
+}
