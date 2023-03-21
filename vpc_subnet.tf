1e23f7984687698f73e09e030142f1ad54b5397a
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-21 14:54:47 +0900

    modify

--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -1,10 +1,12 @@
 resource "aws_subnet" "first" {
   vpc_id     = aws_vpc.backend.id
+  availability_zone = "ap-northeast-2a"
   cidr_block = "10.0.0.0/20"
 }
 
 resource "aws_subnet" "second" {
   vpc_id     = aws_vpc.backend.id
+  availability_zone = "ap-northeast-2c"
   cidr_block = "10.0.16.0/20"
 }
 
