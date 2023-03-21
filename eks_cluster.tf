14222c7a7206937d16efa20fb0be6316331c2142
YoungHo Kwak <jason.uphiller@gmail.com>
2023-03-21 10:55:14 +0900

    Update eks_cluster.tf

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "shop" {
   name    = "shop"
-  role_arn = "arn:aws:iam::193945198166:role/EKS"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [aws_subnet.first.id, aws_subnet.second.id]
