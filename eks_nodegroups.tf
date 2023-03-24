fc09db864dec1509060a13b209b232d2da950fcb
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-24 14:28:56 +0900

    modify nodegroup

--- eks_nodegroups.tf
+++ eks_nodegroups.tf
@@ -1,16 +1,16 @@
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.shop.name
-  node_group_name = "shop_node"
-  node_role_arn   = aws_iam_role.eks_cluster_role.arn
-  subnet_ids      = [aws_subnet.first.id, aws_subnet.second.id]
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-}
+#resource "aws_eks_node_group" "example" {
+#  cluster_name    = aws_eks_cluster.shop.name
+#  node_group_name = "shop_node"
+#  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+#  subnet_ids      = [aws_subnet.first.id, aws_subnet.second.id]
+#
+#  scaling_config {
+#    desired_size = 1
+#    max_size     = 2
+#    min_size     = 1
+#  }
+#
+#  update_config {
+#    max_unavailable = 1
+#  }
+#}
