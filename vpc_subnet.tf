76b3745fdcda48d979b4ce5b7b48dfba5bbc1964
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-24 15:53:09 +0900

    modify subnet

--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -2,12 +2,14 @@ resource "aws_subnet" "first" {
   vpc_id     = aws_vpc.backend.id
   availability_zone = "ap-northeast-2a"
   cidr_block = "10.0.0.0/20"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "second" {
   vpc_id     = aws_vpc.backend.id
   availability_zone = "ap-northeast-2c"
   cidr_block = "10.0.16.0/20"
+  map_public_ip_on_launch = true
 }
 
 output "subnet1" {
