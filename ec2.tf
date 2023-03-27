ea89d037e405ac965554e3f200e82650c5b8f381
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-27 16:00:12 +0900

    add ec2

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,16 @@
-resource "aws_network_interface" "foo" {
+resource "aws_network_interface" "ni" {
   subnet_id   = aws_subnet.first.id
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-03221589fd7c8f183"
   instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.ni.id
+    device_index         = 0
+  }
+
+  key_name = "aws-go"
+
 }
