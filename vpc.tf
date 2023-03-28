d97afb72e2f573670070356b69c54cac8c0e2fcb
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-28 12:58:40 +0900

    modify all

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,8 @@
 resource "aws_vpc" "backend" {
   cidr_block = "10.0.0.0/16"
 }
+
+output "default_route_table_id" {
+  description = "The ID of the default route table"
+  value       = try(aws_vpc.backend[0].default_route_table_id, "")
+}
--- vpc_igw.tf
+++ vpc_igw.tf
@@ -1,3 +1,8 @@
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.backend.id
 }
+
+resource "aws_route_table_association" "b" {
+  gateway_id     = aws_internet_gateway.main.id
+  route_table_id = default_route_table_id
+}
