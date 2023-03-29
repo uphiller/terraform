dcff00f45a7cd85a46fea0734a4770408e33186e
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-29 14:17:53 +0900

    update eks node

--- iam_role.tf
+++ iam_role.tf
@@ -18,9 +18,9 @@ resource "aws_iam_role" "eks_cluster_role" {
 })
 
 managed_policy_arns = [
-"arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-"arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+  "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+  "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 ]
 
 }
@@ -59,4 +59,9 @@ resource "aws_iam_role" "eks_node_role" {
       }
     ]
   })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
 }
