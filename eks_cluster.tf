a2a8c651d5d371e963cda4872bb47ce81306faee
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-29 13:38:07 +0900

    update eks

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,8 +1,8 @@
-#resource "aws_eks_cluster" "shop" {
-#  name    = "shop"
-#  role_arn = aws_iam_role.eks_cluster_role.arn
-#
-#  vpc_config {
-#    subnet_ids = [aws_subnet.first.id, aws_subnet.second.id]
-#  }
-#}
+resource "aws_eks_cluster" "shop" {
+  name    = "shop"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.first.id, aws_subnet.second.id]
+  }
+}
--- eks_nodegroups.tf
+++ eks_nodegroups.tf
@@ -1,7 +1,7 @@
 #resource "aws_eks_node_group" "example" {
 #  cluster_name    = aws_eks_cluster.shop.name
 #  node_group_name = "shop_node"
-#  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+#  node_role_arn   = aws_iam_role.eks_node_role.arn
 #  subnet_ids      = [aws_subnet.first.id, aws_subnet.second.id]
 #
 #  scaling_config {
