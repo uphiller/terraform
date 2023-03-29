9fcc2c293f102b9203f965a49ad33b2f66e45a2c
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-29 14:47:27 +0900

    update eks node

--- iam_role.tf
+++ iam_role.tf
@@ -35,27 +35,17 @@ resource "aws_iam_role" "eks_node_role" {
         "Effect": "Allow",
         "Principal": {
           "Service": [
+            "eks.amazonaws.com",
             "ec2.amazonaws.com"
           ]
         },
-        "Action": [
-          "ec2:AssignPrivateIpAddresses",
-          "ec2:AttachNetworkInterface",
-          "ec2:CreateNetworkInterface",
-          "ec2:DeleteNetworkInterface",
-          "ec2:DescribeInstances",
-          "ec2:DescribeTags",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DetachNetworkInterface",
-          "ec2:ModifyNetworkInterfaceAttribute",
-          "ec2:UnassignPrivateIpAddresses"
-        ],
+        "Action": "sts:AssumeRole"
       }
     ]
   })
 
   managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ]
