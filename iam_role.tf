4f6c713ed20cf54fb76a50311a92704c195ab573
youngho.kwak <youngho.kwak@partner.samsung.com>
2023-03-29 14:13:52 +0900

    update eks node

--- iam_role.tf
+++ iam_role.tf
@@ -25,29 +25,38 @@ managed_policy_arns = [
 
 }
 
-#resource "aws_iam_role" "eks_node_role" {
-#  name = "eks_node_role"
-#
-#  assume_role_policy = jsonencode({
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Effect": "Allow",
-#        "Principal": {
-#          "Service": [
-#            "eks.amazonaws.com",
-#            "ec2.amazonaws.com"
-#          ]
-#        },
-#        "Action": "sts:AssumeRole"
-#      }
-#    ]
-#  })
-#
-#  managed_policy_arns = [
-#    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-#    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-#    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#  ]
-#
-#}
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:AttachNetworkInterface",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DetachNetworkInterface",
+          "ec2:ModifyNetworkInterfaceAttribute",
+          "ec2:UnassignPrivateIpAddresses"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ec2:CreateTags"
+        ],
+        "Resource": [
+          "arn:aws:ec2:*:*:network-interface/*"
+        ]
+      }
+    ]
+  })
+}
